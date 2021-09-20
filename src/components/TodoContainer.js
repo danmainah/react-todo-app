e9fad96b75290b2d38503782257b716aa8631ed0
unknown <danielmainawaweru@gmail.com>
2021-09-20 17:40:22 +0300

    Add componentDidMount function amd load data from apimock

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,25 +4,15 @@ import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
-    state = {
-        todos: [
-          {
-            id: uuidv4(),
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: uuidv4(),
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: uuidv4(),
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
-       };
+      state = {
+        todos: [],
+      }
+     
+      componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+        .then(response => response.json())
+        .then(data => this.setState({ todos: data }));
+      }
 
        handleChange = id => {
         this.setState(prevState => ({
