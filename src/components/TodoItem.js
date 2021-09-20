a2ea34138ad1409d25e624219d7f6dc691c29149
unknown <danielmainawaweru@gmail.com>
2021-09-20 17:26:19 +0300

    Add Edit Todos function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,32 @@
 import React from "react";
 import styles from "./TodoItem.module.css"
 class TodoItem extends React.Component{
+    state = {
+        editing: false,
+      }
+
+     handleEditing = () => {
+        this.setState({
+          editing: true,
+        })
+      }
+
+      handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+          this.setState({ editing: false })
+        }
+      }
+
     render() {
+        let viewMode = {}
+        let editMode = {}
+
+        if (this.state.editing) {
+          viewMode.display = "none"
+        } else {
+         editMode.display = "none"
+        }
+
         const completedStyle = {
             fontStyle: "italic",
             color: "#595959",
@@ -13,14 +38,26 @@ class TodoItem extends React.Component{
 
           return (
             <li className={styles.item}>
+              <div onDoubleClick={this.handleEditing} style={viewMode}>
+                  <input
+                   type="checkbox"
+                   className={styles.checkbox}
+                   checked={completed}
+                   onChange={() => this.props.handleChangeProps(id)}
+                 />
+                 <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+                 <span style={completed ? completedStyle : null}>{title}</span>
+              </div>
               <input
-                type="checkbox"
-                className={styles.checkbox}
-                checked={completed}
-                onChange={() => this.props.handleChangeProps(id)}
-              />
-              <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-              <span style={completed ? completedStyle : null}>{title}</span>
+               type="text"
+               style={editMode}
+               className={styles.textInput}
+              value={title}
+              onChange={e => {
+                this.props.setUpdate(e.target.value, id)
+              }}
+              onKeyDown={this.handleUpdatedDone}
+             />
             </li>
           )
     }
