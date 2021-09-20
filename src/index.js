208dca2b0a61682052a236b093089d412474db31
unknown <danielmainawaweru@gmail.com>
2021-09-20 13:02:19 +0300

    Pass data from todocontainer to todoitem through todolists and display the list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
 import TodoContainer from "./components/TodoContainer"
 
-ReactDOM.render(<TodoContainer />,document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
