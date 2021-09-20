3981870d2b8b48a5d1f223ccae9c0741c104b691
unknown <danielmainawaweru@gmail.com>
2021-09-20 15:54:46 +0300

    Adding a text input field and a submit button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,6 +3,9 @@ class TodoItem extends React.Component{
     render() {
         return <li> <input type="checkbox" checked={this.props.todo.completed}
         onChange={() => this.props.handleChangeProps(this.props.todo.id)}/> 
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+         Delete
+        </button>
         {this.props.todo.title}</li>
     }
 }
